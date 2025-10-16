b52c1705c738703ae98824fcbcf15b055bc3500b
szumaster3 <szumaster2009@protonmail.com>
2025-10-16 02:01:13 +0200

    16.10.2025

--- game/data/botdata/getnames.sh
+++ game/data/botdata/getnames.sh
@@ -1,4 +1,9 @@
-for i in {6000..6500}
+#!/bin/bash
+for i in {6000..6400}
 do
-    wget -O- "https://secure.runescape.com/m=hiscore_oldschool/overall.ws?table=0&page=$i" | grep user1= | sed 's/^.*user1=//'| sed 's/".*$//' | grep -v "Binary" >> botnames.txt
+    curl -s "https://secure.runescape.com/m=hiscore_oldschool/overall.ws?table=0&page=$i" \
+        | grep user1= \
+        | sed 's/^.*user1=//' \
+        | sed 's/".*$//' \
+        | grep -v "Binary" >> botnames.txt
 done
